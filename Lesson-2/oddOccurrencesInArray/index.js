5dc6400c698bbf61d76315ed44a84b3ce12b739c
Amir <amirhatam136@gmail.com>
2022-06-21 21:55:19 +0200

    Lesson 2, OddOccurrencesInArray, solution fix bug

--- Lesson-2/oddOccurrencesInArray/index.js
+++ Lesson-2/oddOccurrencesInArray/index.js
@@ -1,6 +1,6 @@
 const unique = (arr) => {
 
-    return arr.filter(e => arr.indexOf(e) === arr.lastIndexOf(e));
+    return parseInt(arr.filter(e => arr.indexOf(e) === arr.lastIndexOf(e)))
 }
 
 console.log(unique([9, 3, 9, 3, 9, 7, 9]));
