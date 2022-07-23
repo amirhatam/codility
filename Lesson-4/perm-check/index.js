9812e1bd19ff19c32b2321986c232ac345b13c2e
Amir <amirhatam136@gmail.com>
2022-07-23 17:54:25 +0200

    Lesson 4, PermCheck, Seconde Solution, Total score:75 %

--- Lesson-4/perm-check/index.js
+++ Lesson-4/perm-check/index.js
@@ -1,15 +1,30 @@
 // First Solution, Total score: 33 % 
-const solution = (a) => {
+const solutionA = (a) => {
     a.sort()
-    let missingNum = 1
+    let missingNum
 
     for (let i = 1; i <= a.length; i++) {
         if (a.indexOf(i) == -1) {
-            missingNum = missingNum - 1
+            missingNum = 0
+        } else {
+            missingNum = 1
         }
     }
 
     return missingNum
+}
+
+
+
+// Seconde Solution, Total score: 75 % 
+const solution = (a) => {
+    a.sort()
+
+    for (let i = 1; i <= a.length; i++) {
+        if (a.indexOf(i) == -1)
+            return 0;
+    }
+    return 1
 }
 
 console.log(solution([4, 1, 3]));
