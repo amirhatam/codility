9fe033b6c7e98f939eb7952a32e0ac17e4736995
Amir <amirhatam136@gmail.com>
2022-07-24 16:34:36 +0200

    Lesson 5, PassingCars, Solution B, Total score 100%

--- Lesson-5/passing-cars/index.js
+++ Lesson-5/passing-cars/index.js
@@ -12,5 +12,29 @@ const solutionA = (A) => {
     }
     return res
 }
+// console.log(solutionA([0, 1, 0, 1, 1, 2]));
 
-console.log(solutionA([0, 1, 0, 1, 1]));
+
+//Solution B, Total score 100%
+const solutionB = (A) => {
+
+    let counter = 0;
+    let zeroCount = 0;
+
+    let i = A.indexOf(0);
+
+    for (i; i < A.length; i++) {
+        if (A[i] === 0)
+            zeroCount++;
+        else if (A[i] === 1) {
+            counter += zeroCount
+            if (counter > 1000000000)
+                return -1;
+        }
+    }
+
+    return counter;
+}
+
+console.log(solutionB([0, 1, 0, 1, 1]));
+
